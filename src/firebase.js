2329f1d95693e86861fe427e369dc3574fb767e5
William Monroy <william_monroy777@hotmail.com>
2020-09-16 10:25:30 -0500

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCCKghq3rtazrYmI1UhbGmGCFYXL4GAjzk",
+    authDomain: "newsletter-saitc.firebaseapp.com",
+    databaseURL: "https://newsletter-saitc.firebaseio.com",
+    projectId: "newsletter-saitc",
+    storageBucket: "newsletter-saitc.appspot.com",
+    messagingSenderId: "573165132379",
+    appId: "1:573165132379:web:ec787050d448babbfebd7e",
+    measurementId: "G-RFD2LGLGEZ"
+  };
